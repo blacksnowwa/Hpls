3379ca378bb4f6e7e9ef9b96bb3b2808c1c387d3
อธิวัตร เชยโนนสูง <atiwat.cns@gmail.com>
2021-06-20 21:20:53 +0700

    admin page

---
+++ pages/admin.vue
@@ -0,0 +1,55 @@
+<template>
+  <el-table :data="tableData" stripe style="width: 100%">
+    <el-table-column prop="date" label="วันที่" width="180"> </el-table-column>
+    <el-table-column prop="username" label="ห้อง" width="180">
+    </el-table-column>
+    <el-table-column prop="item" label="รายการ"> </el-table-column>
+    <el-table-column prop="send" label="ส่ง"> </el-table-column>
+    <el-table-column prop="recive" label="รับ"> </el-table-column>
+    <el-table-column prop="out" label="ค้าง"> </el-table-column>
+  </el-table>
+</template>
+
+<script>
+import moment from "moment";
+export default {
+  layout: "left",
+  data() {
+    return {
+      tableData: [
+        {
+          date: "2016-05-03",
+          name: "Tom",
+          address: "No. 189, Grove St, Los Angeles"
+        },
+        {
+          date: "2016-05-02",
+          name: "Tom",
+          address: "No. 189, Grove St, Los Angeles"
+        },
+        {
+          date: "2016-05-04",
+          name: "Tom",
+          address: "No. 189, Grove St, Los Angeles"
+        },
+        {
+          date: "2016-05-01",
+          name: "Tom",
+          address: "No. 189, Grove St, Los Angeles"
+        }
+      ]
+    };
+  },
+  async asyncData({ app }) {
+    try {
+      const table = await app.$axios.$get("statement")
+      return {
+        tableData: table.map(x=>{
+            x.date = moment(x.date).format('YYYY-MM-DD')
+            return x
+        })
+      };
+    } catch (error) {}
+  }
+};
+</script>
